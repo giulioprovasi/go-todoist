96e6d7f30ced76adab4a66e35ddaffb80bed79c4
Giulio Provasi <kitensei@gmail.com>
2017-04-10 17:11:40 +0000

    Box path is now env related

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"net/http"
 	"github.com/kitensei/go-todoist/server"
 	"github.com/GeertJohan/go.rice"
+	"os"
 )
+
+var boxPrefix = getenv("BOXPATH", "")
 
 func main() {
 	server.RegisterHandlers()
-	http.Handle("/", http.FileServer(rice.MustFindBox("static").HTTPBox()))
+	http.Handle("/", http.FileServer(rice.MustFindBox(boxPrefix + "static").HTTPBox()))
 	http.ListenAndServe(":8080", nil)
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
