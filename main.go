4c863ae55aea4c7a4f757f1709c5abfdc2109252
Giulio Provasi <kitensei@gmail.com>
2017-04-10 15:18:52 +0000

    Renamed main file

--- main.go
+++ main.go
@@ -4,14 +4,10 @@ import (
 	"net/http"
 	"todo/server"
 	"github.com/GeertJohan/go.rice"
-	"log"
-	"path/filepath"
-	"os"
 )
 
 func main() {
 	server.RegisterHandlers()
 	http.Handle("/", http.FileServer(rice.MustFindBox("static").HTTPBox()))
-	log.Println(filepath.Dir(os.Args[0]))
 	http.ListenAndServe(":8080", nil)
 }
